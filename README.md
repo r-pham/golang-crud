1b1165bac2cc4d7fbb4c78801fbaf2f82da7bee2
Ryan Pham <phamwhich@gmail.com>
2023-05-26 21:23:53 -0700

    Create foundation for gin framework

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # golang-crud
+<<<<<<< Updated upstream
+=======
+
+Simple golang CRUD backend API to handle movie objects
+
+Run `go build` then `go run main.go` to run a local server
+
+Consider removing CompileDaemon
+>>>>>>> Stashed changes
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"golang-crud/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
+
+func main() {
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
+}
